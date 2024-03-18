095a405401ee78f0e6fdbdbed52871c0f8cc1edf
elias.elgarten@gmail.com <eliaselgarten@eliass-mbp.myfiosgateway.com>
2024-03-18 19:05:42 -0400

    remove comments and logs

--- client/src/app/coaches/[coachId]/page.tsx
+++ client/src/app/coaches/[coachId]/page.tsx
@@ -5,10 +5,8 @@ import { fetchCoachSlots, getCompletedCalls } from '@/utils/fetch'
 import React from 'react'
 
 export default async function CoachDash({ params }: { params: { coachId: string } }) {
-  // useSWR could be useful here to refetch
   const coachSlots = sortSlots((await fetchCoachSlots(params.coachId)).filter(slot => !slot.isCompleted))
   const completedCalls = (await getCompletedCalls(params.coachId)).sort((a, b) => new Date(a.startTime).valueOf() - new Date(b.startTime).valueOf())
-  console.log('complete', completedCalls)
 
   return (
     <PageProvider>
